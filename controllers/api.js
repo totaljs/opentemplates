1691b7c9a3377c130718c4937a2198b74d67281f
Peter Širka <petersirka@gmail.com>
2023-02-12 10:34:19 +0100

    Added uploading files.

--- controllers/api.js
+++ controllers/api.js
@@ -1,9 +1,11 @@
 exports.install = function() {
 
 	ROUTE('+POST    /', http, [60 * 5000], 1024); // 5 min. timeout + 1024 kB data
+	ROUTE('+POST    /api/upload/', upload, 1024);
 
 	// Index
 	ROUTE('GET /', index);
+	ROUTE('FILE /download/*.*', download);
 };
 
 function index() {
@@ -23,4 +25,25 @@ function http() {
 		payload.data = payload.data.parseJSON(true);
 
 	FUNC.render(payload, $);
+}
+
+function upload() {
+
+	var $ = this;
+	var file = $.files[0];
+
+	if (file) {
+		file.fs('files', UID(), $.successful(function(response) {
+			response.url = $.hostname('/download/{id}.{ext}'.args(response));
+			$.json(response);
+		}));
+	} else
+		$.json(null);
+
+}
+
+function download(req, res) {
+	var filename = req.split[1];
+	var index = filename.lastIndexOf('.');
+	res.filefs('files', filename.substring(0, index), !!req.query.download);
 }
